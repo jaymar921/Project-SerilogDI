d5497380dcc3baecc22a562cc4937cd558d704f2
jaymar921 <jayharronabejar@gmail.com>
2026-06-21 19:55:05 +0800

    Implement Log Evac with Hangfire and update appsettings for scheduling

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Hangfire;
+using Hangfire.MemoryStorage;
+using LogEvac.Extensions;
 using Project_SerilogDI.helpers;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +14,24 @@ builder.Services.AddSwaggerGen();
 
 builder.InitializeSerilog();
 
+// Log Evac Pre-requisites
+builder.Services.AddHangfire(config =>
+{
+    // Note: For production scenarios, consider using a more robust storage option like SQL Server, Redis, or PostgreSQL instead of in-memory storage.
+    config.SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+          .UseSimpleAssemblyNameTypeSerializer()
+          .UseRecommendedSerializerSettings()
+          .UseMemoryStorage();
+});
+builder.Services.AddHangfireServer();
+
+// Implement Log Evac
+builder.InitializeLogEvac();
+
 var app = builder.Build();
+
+// Log Evac Pre-requisites: Hangfire Dashboard
+app.UseHangfireDashboard();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -25,5 +45,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Run Log Evac Scheduler
+// Depending on the appsettings.json [LogEvacSettings] configuration, this will schedule the Log Evac job to run at the specified intervals (e.g., every hour).
+app.ScheduleLogEvacJob();
 
 app.Run();
